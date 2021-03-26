fc3a6a8f316dc89b39b034c772a02592d99e242d
Pavel Sher <pavel.sher@gmail.com>
2021-03-26 16:42:44 +0100

    Update settings.kts
    add build type

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,5 +3,16 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 version = "2020.2"
 
 project {  
+  
+  buildType{
+    name = "Hello"
+
+    steps {
+        script {
+            scriptContent = "Hello!"
+        }
+    }
+  }
+  
 }
 
